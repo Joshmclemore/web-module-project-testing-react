0961aefe8a65de54d15c9efa3b427168c9706db9
Joshmclemore <josh_mclemore@yahoo.com>
2022-03-28 16:20:55 -0500

    Episode tests are written and passing. Third tests directions did not match component img alt tag

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,40 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testEpisode = {
+    id: 1,
+    name: "test name",
+    image: 'https://i.ibb.co/2FsfXqM/stranger-things.png',
+    season: 1,
+    number: 1,
+    summary: "test summary",
+    runtime: 1
+}
 
-test("renders the summary test passed as prop", () => { });
+const testEpisodeWithNullImage = {
+    id: 1,
+    name: "test name",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "test summary",
+    runtime: 1
+}
 
-test("renders default image when image is not defined", () => { });
+test("renders without error", () => {
+    render(<Episode episode={testEpisode} />);
+});
+
+test("renders the summary test passed as prop", () => { 
+    render(<Episode episode={testEpisode} />);
+    const summary = screen.queryByText('test summary', { exact: false })
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).toHaveTextContent('test summary')
+ });
+
+test("renders default image when image is not defined", () => { 
+    render(<Episode episode={testEpisodeWithNullImage} />);
+    const altTag = screen.queryAllByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png', { exact: false })
+    console.log(altTag)
+ });
